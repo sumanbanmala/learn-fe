50f638f7926a113fa50291c6241d6e6c9cf0ffd3
sbanmala <sumanbanmala12@gmail.com>
2025-07-29 21:20:51 +0545

    modified nav

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let logos=document.getElementsByClassName("logo");
-console.dir(logos);
+// let logos=document.getElementsByClassName("logo");
+// console.dir(logos);
 let navs=document.getElementsByClassName("nav");
 console.dir(navs);
-logos.addEventListener('click',()=>{
-    navs.style.backgroundColor = "lightblue";
+// // logos.addEventListener('click',()=>{
+// //     navs.style.backgroundColor = "lightblue";
 
-})
+// // })
+// window.addEventListener("scroll",()=>{
+//     navs.style.backgroundColoe("blue")
+// })
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("bg-gray-500");
+    } else {
+      navbar.classList.remove("bg-gray-500");
+    }
+  });
